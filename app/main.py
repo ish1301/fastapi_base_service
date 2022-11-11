7717be43fa3e818aeec87061e9530ad0980c7aaf
Ish Kumar <ish.kumar@netlync.com>
2022-11-11 15:24:39 -0500

    Add app folder

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,7 @@ app = FastAPI()
 @app.get("/{name}")
 async def home(name: str):
     return {"message": "Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:main", host="127.0.0.1", port=6000, reload=True)
