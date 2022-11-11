ef5b576375de3e39f25584b6a3882e8885669210
Ish Kumar <ish.kumar@netlync.com>
2022-11-11 17:19:26 -0500

    Add crud actions

---
+++ app/claims/crud.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_claim(db: Session, claim_id: int):
+    return db.query(models.Claim).filter(models.Claim.id == claim_id).first()
+
+
+def get_claims(db: Session, skip: int = 0, limit: int = 10):
+    limit = 100 if limit > 100 else limit
+    return db.query(models.Claim).offset(skip).limit(limit).all()
+
+
+def create_claim(db: Session, claim: schemas.ClaimCreate):
+    claimData = {
+        "name": claim.name,
+        "description": claim.description,
+        "price": claim.price,
+        "tax": claim.tax,
+    }
+
+    db_claim = models.Claim(*claimData)
+    db.add(db_claim)
+    db.commit()
+    db.refresh(db_claim)
+    return db_claim
--- app/claims/main.py
+++ app/claims/main.py
@@ -1,10 +1,23 @@
 import uvicorn
 
+
 from fastapi import FastAPI
+from app.claims import crud, models, schemas
+from app.claims.database import SessionLocal, engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/claims")
 async def get_claims():
-    return {"message": f"Hello World"}
+    return []
