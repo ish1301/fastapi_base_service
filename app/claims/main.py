9434b4108a1ccf084d7232d494939ae1f4185f3b
Ish Kumar <ish.kumar@netlync.com>
2022-11-11 17:58:28 -0500

    Create claims

--- app/claims/main.py
+++ app/claims/main.py
@@ -23,6 +23,12 @@ async def list_claims(skip: int = 0, limit: str = 10, db: Session = Depends(get_
     return claims
 
 
+@app.get("/claims/{claim_id}", response_model=schemas.Claim)
+async def get_claim(claim_id: int, db: Session = Depends(get_db)):
+    claims = crud.get_claim(db, claim_id=claim_id)
+    return claims
+
+
 @app.post("/claims/", response_model=schemas.Claim)
 def create_claim(claim: schemas.ClaimCreate, db: Session = Depends(get_db)):
     return crud.create_claim(db=db, claim=claim)
