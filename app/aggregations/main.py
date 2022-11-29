ed07de73e36e518bf0dcfc23a74c0efa9f4c443f
Ish Kumar <ish.kumar@netlync.com>
2022-11-29 18:10:51 -0500

    Cleanup

--- app/aggregations/main.py
+++ app/aggregations/main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI, HTTPException, UploadFile
 app = FastAPI()
 
 
-@app.post("/upload_network_events/", response_model=list[NetworkEventBase])
+@app.post("/network_events/", response_model=list[NetworkEventBase])
 async def upload_network_events(records: list[NetworkEventCreate]):
+    """
+    Submit network events to our data stream
+    """
+    for i in records:
+        print(i)
     return records
