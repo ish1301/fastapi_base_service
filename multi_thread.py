a85420ed2bf0789c8d13f9042019254921d9a1c6
Ish Kumar <ish.kumar@netlync.com>
2024-07-26 09:05:53 -0400

    add multi threads

--- multi_thread.py
+++ multi_thread.py
@@ -1,32 +1,32 @@
 
 import time
-from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 
 def getfilenames():
     return [f'{i}.txt' for i in range(10)]
 
 def readlines(filename):
     print(f'processing {filename}')
+    # time.sleep(1)
     return [f'line {i}'for i in range(10**6)]
 
 
 if __name__ == "__main__":
     t1 = time.perf_counter(), time.process_time()
     threads = []
-    for filename in getfilenames():
-        print(f"{filename}")
-        threads.append(Thread(target=readlines, args=(filename,)))
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        for filename in getfilenames():
+            print(f"{filename}")
+            threads.append(executor.submit(readlines, filename))
 
+    total = 0
     for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
+        total += len(t.result())
 
     t2 = time.perf_counter(), time.process_time()
 
     print()
     print(f"Real time: {t2[0] - t1[0]:.2f} seconds")
     print(f"CPU time: {t2[1] - t1[1]:.2f} seconds")
-    # print(f"Total lines: {total:,}")
+    print(f"Total lines: {total:,}")
     print()
